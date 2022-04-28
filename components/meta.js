de1a8d199c9f0a9e28141b3fc1fda186468fc4da
tzouyuhsin <yhtzou@gmail.com>
2022-04-28 22:38:03 +0800

    2022-04-28-02

--- components/meta.js
+++ components/meta.js
@@ -7,29 +7,29 @@ export default function Meta() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicons/apple-touch-icon.png"
+        href="/static/favicons/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicons/favicon-32x32.png"
+        href="/static/favicons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicons/favicon-16x16.png"
+        href="/static/favicons/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicons/safari-pinned-tab.svg"
+        href="/static/favicons/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+      <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
--- components/nav.js
+++ components/nav.js
@@ -4,7 +4,7 @@ import Container from './container'
 export default function Nav(){
   return (
     <Container>
-      <div class='-ml-3 mt-4 mb-12 flex justify-center md:justify-start'>
+      <div className='-ml-3 mt-4 mb-12 flex justify-center md:justify-start'>
         <a
           href='/'
           className='text-base rounded-lg hover:bg-slate-100 duration-200 px-4 py-2'
